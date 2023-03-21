5c29d49243863c50a28008602c1c1e4282e13d1b
Molnar Mate <molnar.mate@evista.hu>
2023-03-21 16:19:01 +0100

    vue3 init

---
+++ src/components/ModalContent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    <p>{{ msg }}</p>
+    <button @click="$emit('close')">Close</button>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: String,
+  msg: String
+})
+
+defineEmits('close')
+</script>
---
+++ src/components/NotificationHandler.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="root">
+    <button @click="isOpen = true">Open</button>
+
+    <teleport to="body">
+      <div class="modal" v-if="isOpen">
+        <modal-content @close="isOpen = false" title="Does this work?" msg="I hope so." />
+      </div>
+    </teleport>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import ModalContent from './ModalContent.vue'
+
+const isOpen = ref(false)
+</script>
+
+<style>
+.root {
+  position: relative;
+}
+
+.modal {
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.1);
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal > div {
+  background-color: #fff;
+  padding: 50px;
+  border-radius: 10px;
+}
+</style>
